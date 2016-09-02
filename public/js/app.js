cf48fa45dfbcc6fc58b68427a38733a34e42fb1d
Jonathan <theflametrooper@gmail.com>
2016-09-02 19:02:53 +0100

    Minor Styling Improvements to Room Cards

--- public/js/app.js
+++ public/js/app.js
@@ -38,20 +38,20 @@ window.onload = function () {
 					id: "lu08gw56571r4497wrk9",
 					name: "Official Rooms",
 					rooms: [
-						{ id: "73qvw65746acvo8yqfr", name: "Country", description: "Country Music", users: 10 },
-						{ id: "enxcysmhn1k7ld56ogvi", name: "Pop", description: "Pop Music", users: 14 },
-						{ id: "kqa99gbva7lij05dn29", name: "Chill", description: "Chill Music", users: 13 },
-						{ id: "w19hu791iiub6wmjf9a4i", name: "EDM", description: "EDM Music", users: 13 }
+						{ id: "73qvw65746acvo8yqfr", thumbnail: "https://lh6.googleusercontent.com/-ghASz3s6yL4/AAAAAAAAAAI/AAAAAAAAALc/tFblPp2myu0/s0-c-k-no-ns/photo.jpg", name: "Country", description: "Johnny Cash - I Walk The Line", users: 10 },
+						{ id: "enxcysmhn1k7ld56ogvi", thumbnail: "http://66.media.tumblr.com/1734069af425e491fae7deae0a19869f/tumblr_o0i0xmIYrF1v421f2o1_1280.jpg", name: "Pop", description: "Sia - Cheap Thrills", users: 14 },
+						{ id: "kqa99gbva7lij05dn29", thumbnail: "http://www.youredm.com/wp-content/uploads/2014/09/taking-you-higher.jpg", name: "Chill", description: "MrSuicideSheep - Taking you higher", users: 13 },
+						{ id: "w19hu791iiub6wmjf9a4i", thumbnail: "http://edmsauce.wpengine.netdna-cdn.com/wp-content/uploads/2012/12/Deadmau5-album-title-goes-here.jpg", name: "EDM", description: "Deadmau5 - There Might Be Coffee", users: 13 }
 					]
 				},
 				{
 					id: "g2b8v03xaedj8ht1emi",
 					name: "Trending Rooms",
 					rooms: [
-						{ id: "73qvw65746acvo8yqfr", name: "Country", description: "Country Music", users: 10 },
-						{ id: "enxcysmhn1k7ld56ogvi", name: "Pop", description: "Pop Music", users: 14 },
-						{ id: "kqa99gbva7lij05dn29", name: "Chill", description: "Chill Music", users: 13 },
-						{ id: "w19hu791iiub6wmjf9a4i", name: "EDM", description: "EDM Music", users: 13 }
+						{ id: "73qvw65746acvo8yqfr", thumbnail: "https://lh6.googleusercontent.com/-ghASz3s6yL4/AAAAAAAAAAI/AAAAAAAAALc/tFblPp2myu0/s0-c-k-no-ns/photo.jpg", name: "Country", description: "Johnny Cash - I Walk The Line", users: 10 },
+						{ id: "enxcysmhn1k7ld56ogvi", thumbnail: "http://66.media.tumblr.com/1734069af425e491fae7deae0a19869f/tumblr_o0i0xmIYrF1v421f2o1_1280.jpg", name: "Pop", description: "Sia - Cheap Thrills", users: 14 },
+						{ id: "kqa99gbva7lij05dn29", thumbnail: "http://www.youredm.com/wp-content/uploads/2014/09/taking-you-higher.jpg", name: "Chill", description: "MrSuicideSheep - Taking you higher", users: 13 },
+						{ id: "w19hu791iiub6wmjf9a4i", thumbnail: "http://edmsauce.wpengine.netdna-cdn.com/wp-content/uploads/2012/12/Deadmau5-album-title-goes-here.jpg", name: "EDM", description: "Deadmau5 - There Might Be Coffee", users: 13 }
 					]
 				}
 			]
