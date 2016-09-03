0a489b460bd5bb8192736416c02313f14e492acf
Cameron Kline <tu4756@gmail.com>
2016-09-03 11:42:28 -0400

    Fixed issues in express handler

--- logic/expressHandler.js
+++ logic/expressHandler.js
@@ -1,27 +1,27 @@
 'use strict';
 
-module.exports = function (base, io) {//TODO Rename base to core
+module.exports = function (core, app) {
 
 	app.post('/login', function (user) {
-		base.login(user, function (result) {
+		core.login(user, function (result) {
 			res.send(JSON.stringify(result));
 		});
 	});
 
 	app.post('/register', function (user) {
-		base.register(user, function (result) {
+		core.register(user, function (result) {
 			res.send(JSON.stringify(result));
 		});
 	});
 
 	app.get('/rooms', function () {
-		base.rooms(function (result) {
+		core.rooms(function (result) {
 			res.send(JSON.stringify(result));
 		});
 	});
 
 	app.get('/search/:query', function () {//TODO Replace search with a better name.
-		base.search(query, function (result) {
+		core.search(query, function (result) {
 			res.send(JSON.stringify(result));
 		});
 	});
