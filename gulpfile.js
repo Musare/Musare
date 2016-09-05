ab15aff17da3f42411887a5e6f4016b5560a06ed
Cameron Kline <tu4756@gmail.com>
2016-09-05 14:49:18 -0400

    Fixed whitespace

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,18 @@ const sass = require('gulp-sass');
 const nodemon = require('gulp-nodemon');
 
 gulp.task('sass', () => {
-  return gulp.src('./src/sass/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+	return gulp.src('./src/sass/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('start', () => {
-  nodemon({
-    script: 'src/app.js',
+	nodemon({
+		script: 'src/app.js',
 		ext: 'js'
-  });
+	});
 });
 
 gulp.task('default', ['start'], () => {
-  gulp.watch('./src/sass/**/*.scss', ['sass']);
+	gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
