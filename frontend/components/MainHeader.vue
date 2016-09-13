2707e705727dbae783d0d5d46311aa9da59653ff
KrisVos130 <krisvos130@gmail.com>
2016-09-13 20:33:19 +0200

    Edited the layout a bit of the files.

---
+++ frontend/components/MainHeader.vue
@@ -0,0 +1,24 @@
+<template>
+	<header>
+		<div class="header-inner header-gray">
+			<div class="title link">Musare</div>
+			<div class="link">Account&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-caret-down" aria-hidden="true"></i></div>
+			<div class="link">Donate</div>
+			<div class="link">The Project</div>
+		</div>
+	</header>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: "Musare ;)"
+			}
+		}
+	}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
