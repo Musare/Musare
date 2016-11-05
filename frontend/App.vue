807db6a7c2cca829b59e1d9e28416af6aad3e50c
Cameron Kline <tu4756@gmail.com>
2016-11-05 03:12:08 -0400

    Added Docker setup, which should eventually replace the Vagrant setup

--- frontend/App.vue
+++ frontend/App.vue
@@ -101,7 +101,7 @@
 		},
 		ready: function () {
 			let local = this;
-			local.socket = io();
+			local.socket = io(window.location.protocol + '//' + window.location.hostname + ':8081');
 			local.socket.on("ready", status => {
 				local.loggedIn = status;
 				local.socket.emit("/user/ratings", result => {
