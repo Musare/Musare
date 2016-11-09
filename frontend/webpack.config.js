22bd87aba09238438e8dd8e9fc2407fe7042b790
Cameron Kline <tu4756@gmail.com>
2016-11-08 23:20:03 -0500

    Removed comments from webpack config. Use consistent quote types

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,6 @@
 // webpack.config.js
 module.exports = {
-	// entry point of our application
 	entry: './main.js',
-	// where to place the compiled bundle
 	output: {
 		path: __dirname + '/build/',
 		filename: 'bundle.js'
@@ -15,20 +13,19 @@ module.exports = {
 				exclude: /node_modules/
 			}
 		],
-		// `loaders` is an array of loaders to use.
 		loaders: [
 			{
-				test: /\.vue$/, // a regex for matching all files that end in `.vue`
-				loader: 'vue'   // loader to use for matched files,
+				test: /\.vue$/,
+				loader: 'vue'
 			},
 			{
-				test: /\.js$/,          // a regex for matching all files that end in `.js`
-				loader: 'babel',        // loader to use for matched files,
-				exclude: /node_modules/ // excludes the folder `node_modules`
+				test: /\.js$/,
+				loader: 'babel',
+				exclude: /node_modules/
 			},
 			{
-				test: /\.scss$/,                 // a regex for matching all files that end in `.scss`
-				loader: "css-loader!sass-loader" // loader to use for matched files,
+				test: /\.scss$/,
+				loader: 'css-loader!sass-loader'
 			}
 		]
 	},
