97d3b577d0e38db4e98034f3dcbacdbc855c733c
theflametrooper <theflametrooper@gmail.com>
2016-12-02 23:50:40 +0000

    Quick syntax fix with frontend/auth.js

--- frontend/auth.js
+++ frontend/auth.js
@@ -7,20 +7,17 @@ export default {
 	username: '',
 	role: 'default',
 
-	getStatus: function(cb) {
-		if (this.ready) {
-			cb(this.authenticated, this.role, this.username);
-		} else {
-			callbacks.push(cb);
-		}
+	getStatus: function (cb) {
+		if (this.ready) cb(this.authenticated, this.role, this.username);
+		else callbacks.push(cb);
 	},
 
-	data: function(authenticated, role, username) {
+	data: function (authenticated, role, username) {
 		this.authenticated = authenticated;
 		this.role = role;
 		this.username = username;
 		this.ready = true;
-		callbacks.forEach((callback) => {
+		callbacks.forEach(callback => {
 			callback(authenticated, role, username);
 		});
 		callbacks = [];
