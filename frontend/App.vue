3939021fefbe882ccea94ed7526580eacad65ed1
theflametrooper <theflametrooper@gmail.com>
2016-12-16 18:21:22 +0000

    When user logs out, they are redirected to /

--- frontend/App.vue
+++ frontend/App.vue
@@ -46,9 +46,11 @@
 		},
 		methods: {
 			logout: function () {
-				this.socket.emit('users.logout', result => {
+				let _this = this;
+				_this.socket.emit('users.logout', result => {
 					if (result.status === 'success') {
 						document.cookie = 'SID=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+						_this.$router.go('/');
 						location.reload();
 					} else Toast.methods.addToast(result.message, 4000);
 				});
