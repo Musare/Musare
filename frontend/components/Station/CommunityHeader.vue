407cc9cdba156525a8080ef05cb739a7ab642cf6
Owen Diffey <owen@odiffey.uk>
2017-04-09 20:19:55 +0100

    Disabled SongsList for community non-partyMode

--- frontend/components/Station/CommunityHeader.vue
+++ frontend/components/Station/CommunityHeader.vue
@@ -99,7 +99,7 @@
 				</a>
 				<hr>
 			</div>
-			<a class="sidebar-item" href='#' @click='$parent.toggleSidebar("songslist")'>
+			<a class="sidebar-item" href='#' @click='$parent.toggleSidebar("songslist")' v-if='$parent.station.partyMode === true'>
 				<span class='icon'>
 					<i class='material-icons'>queue_music</i>
 				</span>
