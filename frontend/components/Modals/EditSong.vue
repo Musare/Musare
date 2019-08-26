6b1d05131c6a9147f161958a1e500a3a476054c9
Kristian Vos <krisvos130@gmail.com>
2019-08-26 22:45:21 +0200

    refactor: small addition to genre helper

--- frontend/components/Modals/EditSong.vue
+++ frontend/components/Modals/EditSong.vue
@@ -1074,6 +1074,7 @@ export default {
 		},
 		toggleGenreHelper() {
 			this.genreHelper.shown = !this.genreHelper.shown;
+			this.saveGenreHelper();
 		},
 		resetGenreHelper() {
 			this.genreHelper.top = 0;
