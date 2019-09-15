2c179fe0c09a6fb7cd619e04b4a1b6302ab73b0e
Kristian Vos <krisvos130@gmail.com>
2019-09-15 11:32:28 +0200

    fix: fixed issue where logo would sometimes jump

--- frontend/components/MainFooter.vue
+++ frontend/components/MainFooter.vue
@@ -39,7 +39,7 @@
 				<a href="/"
 					><img
 						class="musareFooterLogo"
-						src="/assets/wordmark.png"
+						src="/assets/blue_wordmark.png"
 						alt="Musare"
 				/></a>
 				<p class="footerLinks">
--- frontend/components/MainHeader.vue
+++ frontend/components/MainHeader.vue
@@ -3,7 +3,7 @@
 		<div class="nav-left">
 			<router-link class="nav-item is-brand" to="/">
 				<img
-					:src="`${this.siteSettings.logo}`"
+					:src="`${this.siteSettings.logo_white}`"
 					:alt="`${this.siteSettings.siteName}` || `Musare`"
 				/>
 			</router-link>
@@ -142,12 +142,11 @@ export default {
 		font-size: 2.1rem !important;
 		line-height: 38px !important;
 		padding: 0 20px;
-		color: $white;
 		font-family: Pacifico, cursive;
-		filter: brightness(0) invert(1);
 
 		img {
 			max-height: 38px;
+			color: $musareBlue;
 		}
 	}
 
