9804a5e62bb5d607ea48c5e2e8ce0d44972b9787
Kristian Vos <krisvos130@gmail.com>
2021-03-06 21:31:21 +0100

    Small improvement to migration2

--- backend/logic/migration/migrations/migration2.js
+++ backend/logic/migration/migrations/migration2.js
@@ -56,6 +56,7 @@ export default async function migrate(MigrationModule) {
 													includedPlaylists: [],
 													excludedPlaylists: [],
 													playlist: station.type === "official" ? [] : station.playlist,
+													genres: [],
 													documentVersion: 2
 												}
 											},
