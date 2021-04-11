8d62d4e985604e51d533d569e7888fc9eae3ba54
Owen Diffey <owen@diffey.dev>
2021-04-11 14:28:08 +0100

    Improved confirmation component

--- frontend/src/components/Confirm.vue
+++ frontend/src/components/Confirm.vue
@@ -5,12 +5,35 @@
 		theme="confirm"
 		ref="confirm"
 		trigger="click"
+		@hide="confirm(false)"
 	>
 		<template #trigger>
-			<div @click.shift="$emit('confirm')">
+			<div @click.shift="confirm(true)" @click.exact="confirm()">
 				<slot />
 			</div>
 		</template>
-		<a @click.prevent="$emit('confirm')"> Confirm </a>
+		<a @click.exact="confirm(true)"> Click to Confirm </a>
 	</tippy>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			clickedOnce: false
+		};
+	},
+	methods: {
+		confirm(confirm) {
+			if (confirm === false) {
+				this.clickedOnce = false;
+			} else if (confirm === true || this.clickedOnce === true) {
+				this.$emit("confirm");
+				this.clickedOnce = false;
+			} else {
+				this.clickedOnce = true;
+			}
+		}
+	}
+};
+</script>
