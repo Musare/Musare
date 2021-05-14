f4c491f335ef3821c17602f2f5d6d7209549e117
Jonathan <theflametrooper@gmail.com>
2021-05-14 13:40:44 +0100

    fix(Queue): cleaned up code to undo repositioning of song if backend failure
    Signed-off-by: Jonathan <theflametrooper@gmail.com>

--- frontend/src/components/Queue.vue
+++ frontend/src/components/Queue.vue
@@ -240,21 +240,12 @@ export default {
 				},
 				res => {
 					new Toast({ content: res.message, timeout: 4000 });
-					if (res.status !== "success") {
-						if (this.sector === "station") {
-							this.repositionSongInListStation({
-								...moved.element,
-								newIndex: moved.oldIndex,
-								oldIndex: moved.newIndex
-							});
-						} else if (this.sector === "manageStation") {
-							this.repositionSongInListManageStation({
-								...moved.element,
-								newIndex: moved.oldIndex,
-								oldIndex: moved.newIndex
-							});
-						}
-					}
+					if (res.status !== "success")
+						this.repositionSongInList({
+							...moved.element,
+							newIndex: moved.oldIndex,
+							oldIndex: moved.newIndex
+						});
 				}
 			);
 		},
@@ -276,11 +267,16 @@ export default {
 				}
 			});
 		},
-		...mapActions("modals/manageStation", {
-			repositionSongInListManageStation: "repositionSongInList"
-		}),
-		...mapActions("station", {
-			repositionSongInListStation: "repositionSongInList"
+		...mapActions({
+			repositionSongInList(dispatch, payload) {
+				if (this.sector === "manageStation")
+					return dispatch(
+						"modals/manageStation/repositionSongInList",
+						payload
+					);
+
+				return dispatch("station/repositionSongInList", payload);
+			}
 		}),
 		...mapActions("modalVisibility", ["openModal"]),
 		...mapActions({
