1cf40bb64041270681430e26a916d3faf2e21a24
Kristian Vos <krisvos130@gmail.com>
2021-10-22 19:09:02 +0200

    refactor: when deleting your account you'll be redirected to home

--- frontend/src/components/modals/RemoveAccount.vue
+++ frontend/src/components/modals/RemoveAccount.vue
@@ -250,7 +250,7 @@ export default {
 						lofig.get("cookie").then(cookie => {
 							document.cookie = `${cookie.SIDname}=;expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
 							this.closeModal("removeAccount");
-							return window.location.reload();
+							window.location.href = "/";
 						})
 					);
 				}
