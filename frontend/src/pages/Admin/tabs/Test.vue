81a2d95a020b94242c576435a011d5ab667fdc4c
Owen Diffey <owen@diffey.dev>
2021-12-05 19:58:56 +0000

    feat: Added/changed admin test tab columns

--- frontend/src/pages/Admin/tabs/Test.vue
+++ frontend/src/pages/Admin/tabs/Test.vue
@@ -11,16 +11,38 @@
 					/>
 				</template>
 				<template #column-thumbnailUrl="slotProps">
-					{{ slotProps.item.thumbnail }}
+					<a :href="slotProps.item.thumbnail" target="_blank">
+						{{ slotProps.item.thumbnail }}
+					</a>
 				</template>
 				<template #column-_id="slotProps">
 					{{ slotProps.item._id }}
+				</template>
+				<template #column-youtubeId="slotProps">
+					<a
+						:href="
+							'https://www.youtube.com/watch?v=' +
+							`${slotProps.item.youtubeId}`
+						"
+						target="_blank"
+					>
+						{{ slotProps.item.youtubeId }}
+					</a>
 				</template>
 				<template #column-title="slotProps">
 					{{ slotProps.item.title }}
 				</template>
 				<template #column-artists="slotProps">
 					{{ slotProps.item.artists.join(", ") }}
+				</template>
+				<template #column-genres="slotProps">
+					{{ slotProps.item.genres.join(", ") }}
+				</template>
+				<template #column-requestedBy="slotProps">
+					<user-id-to-username
+						:user-id="slotProps.item.requestedBy"
+						:link="true"
+					/>
 				</template>
 			</advanced-table>
 		</div>
@@ -29,17 +51,19 @@
 
 <script>
 import AdvancedTable from "@/components/AdvancedTable.vue";
+import UserIdToUsername from "@/components/UserIdToUsername.vue";
 
 export default {
 	components: {
-		AdvancedTable
+		AdvancedTable,
+		UserIdToUsername
 	},
 	data() {
 		return {
 			columns: [
 				{
 					name: "thumbnailImage",
-					displayName: "Thumbnail (Image)",
+					displayName: "Thumb",
 					properties: ["thumbnail"],
 					sortable: false,
 					filterable: false,
@@ -49,12 +73,24 @@ export default {
 				},
 				{
 					name: "_id",
-					displayName: "Musare Id",
+					displayName: "Musare ID",
 					properties: ["_id"],
 					sortable: true,
 					sortProperty: "_id",
 					filterable: true,
 					filterProperty: "_id",
+					hidable: true,
+					defaultVisibility: "shown",
+					draggable: true
+				},
+				{
+					name: "youtubeId",
+					displayName: "YouTube ID",
+					properties: ["youtubeId"],
+					sortable: true,
+					sortProperty: "youtubeId",
+					filterable: true,
+					filterProperty: "youtubeId",
 					hidable: true,
 					defaultVisibility: "shown",
 					draggable: true
@@ -75,10 +111,22 @@ export default {
 					name: "artists",
 					displayName: "Artists",
 					properties: ["artists"],
-					sortable: true,
+					sortable: false,
 					sortProperty: "artists",
 					filterable: true,
 					filterProperty: "artists",
+					hidable: true,
+					defaultVisibility: "shown",
+					draggable: true
+				},
+				{
+					name: "genres",
+					displayName: "Genres",
+					properties: ["genres"],
+					sortable: false,
+					sortProperty: "genres",
+					filterable: true,
+					filterProperty: "genres",
 					hidable: true,
 					defaultVisibility: "shown",
 					draggable: true
@@ -93,6 +141,18 @@ export default {
 					filterProperty: "thumbnail",
 					hidable: true,
 					defaultVisibility: "hidden",
+					draggable: true
+				},
+				{
+					name: "requestedBy",
+					displayName: "Requested By",
+					properties: ["requestedBy"],
+					sortable: true,
+					sortProperty: "requestedBy",
+					filterable: true,
+					filterProperty: "requestedBy",
+					hidable: true,
+					defaultVisibility: "shown",
 					draggable: true
 				}
 			]
