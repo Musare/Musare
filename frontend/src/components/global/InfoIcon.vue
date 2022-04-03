4b5ebc67950604f58f1d553765f17c032cd49c6c
Owen Diffey <owen@diffey.dev>
2022-04-03 18:04:09 +0100

    feat: Added InfoIcon component

---
+++ frontend/src/components/global/InfoIcon.vue
@@ -0,0 +1,23 @@
+<template>
+	<span class="material-icons info-icon" :content="tooltip" v-tippy>
+		info
+	</span>
+</template>
+
+<script>
+export default {
+	props: {
+		tooltip: {
+			type: String,
+			required: true
+		}
+	}
+};
+</script>
+
+<style lang="less" scoped>
+.material-icons.info-icon {
+	font-size: 14px;
+	margin: auto 5px;
+}
+</style>
