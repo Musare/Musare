da4ab6e261e8f96821f2ee0f9b9a2e406fdc872a
Owen Diffey <owen@diffey.dev>
2022-10-23 06:21:31 +0100

    refactor(musare.sh): Use package.json lint scripts and autoremove markdown lint container

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,13 +2,9 @@
 
 if [[ "${CONTAINER_MODE}" == "dev" ]]; then
     npm install --silent
-fi
-
-if [[ "${BACKEND_MODE}" == "prod" ]]; then
-    if [[ "${CONTAINER_MODE}" == "dev" ]]; then
+    if [[ "${BACKEND_MODE}" == "prod" ]]; then
         npm run build
     fi
-    npm run prod
-elif [ "${BACKEND_MODE}" == "dev" ]; then
-    npm run dev -- --legacy-watch --no-stdin
 fi
+
+npm run "${BACKEND_MODE}"
