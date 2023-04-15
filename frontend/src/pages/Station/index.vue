73966601f5977ec6a0826ae952309c6f5301f54c
Kristian Vos <k.vos@kvos.dev>
2023-04-15 10:32:07 +0200

    refactor: don't enable SoundCloud iframe if SoundCloud isn't enabled

--- frontend/src/pages/Station/index.vue
+++ frontend/src/pages/Station/index.vue
@@ -2204,6 +2204,7 @@ onBeforeUnmount(() => {
 									/>
 								</div>
 								<iframe
+									v-if="experimental.soundcloud"
 									v-show="
 										currentSongMediaType === 'soundcloud'
 									"
