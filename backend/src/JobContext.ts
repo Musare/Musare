06bdd370daae0b244fdaea27e3c43f3859402b72
Owen Diffey <owen@diffey.dev>
2023-06-04 03:01:48 +0100

    feat: Add job context methods to get session user and assert logged in and perms

--- backend/src/JobContext.ts
+++ backend/src/JobContext.ts
@@ -1,3 +1,4 @@
+import { Types } from "mongoose";
 import BaseModule from "./BaseModule";
 import Job from "./Job";
 import JobQueue from "./JobQueue";
@@ -5,6 +6,9 @@ import { Log } from "./LogBook";
 import { SessionSchema } from "./schemas/session";
 import { JobOptions } from "./types/JobOptions";
 import { Jobs, Modules } from "./types/Modules";
+import { StationType } from "./schemas/station";
+import { UserRole, UserSchema } from "./schemas/user";
+import permissions from "./permissions";
 
 export default class JobContext {
 	public readonly job: Job;
@@ -12,6 +16,8 @@ export default class JobContext {
 	public readonly jobQueue: JobQueue;
 
 	private session?: SessionSchema;
+
+	private user?: UserSchema;
 
 	public constructor(job: Job, session?: SessionSchema) {
 		this.job = job;
@@ -65,5 +71,60 @@ export default class JobContext {
 			session: this.session,
 			...(options ?? {})
 		}).execute();
+	}
+
+	public async getUser(refresh = false) {
+		if (!this.session?.userId) throw new Error("No user found for session");
+
+		if (this.user && !refresh) return this.user;
+
+		const User = await this.executeJob("data", "getModel", "user");
+
+		this.user = await User.findById(this.session.userId);
+
+		if (!this.user) throw new Error("No user found for session");
+
+		return this.user;
+	}
+
+	public async assertLoggedIn() {
+		if (!this.session?.userId) throw new Error("No user found for session");
+	}
+
+	public async assertPermission(
+		permission: string,
+		scope?: { stationId?: Types.ObjectId }
+	) {
+		if (!this.session?.userId) throw new Error("Insufficient permissions");
+
+		const user = await this.getUser();
+
+		const roles: (UserRole | "owner" | "dj")[] = [user.role];
+
+		if (scope?.stationId) {
+			const Station = await this.executeJob(
+				"data",
+				"getModel",
+				"station"
+			);
+
+			const station = await Station.findById(scope.stationId);
+
+			if (
+				station.type === StationType.COMMUNITY &&
+				station.owner === this.session.userId
+			)
+				roles.push("owner");
+			if (station.djs.find(dj => dj === this.session?.userId))
+				roles.push("dj");
+		}
+
+		let hasPermission;
+		roles.forEach(role => {
+			if (permissions[role] && permissions[role][permission])
+				hasPermission = true;
+		});
+
+		if (!hasPermission) throw new Error("Insufficient permissions");
 	}
 }
