ac4e0d12ef693940817b08f9feae83245c07fe15
Owen Diffey <owen@diffey.dev>
2025-05-20 18:31:49 +0100

    feat: Add sidebar component

---
+++ frontend/src/pages/NewStation/Components/Sidebar.vue
@@ -0,0 +1,27 @@
+<template>
+	<nav class="sidebar">
+		<slot />
+	</nav>
+</template>
+
+<style lang="less" scoped>
+.sidebar {
+	display: flex;
+	flex: 0 0 350px;
+	flex-direction: column;
+	min-height: 100%;
+	max-height: calc(100vh - 64px);
+	background-color: var(--white);
+	padding: 20px;
+	gap: 20px;
+	box-shadow: 0px 0px 2px 1px rgba(10, 10, 10, 0.1);
+	overflow: auto;
+
+	:deep(&__divider) {
+		background-color: var(--light-grey-2);
+		border-radius: 10px;
+		height: 5px;
+		flex-shrink: 0;
+	}
+}
+</style>
