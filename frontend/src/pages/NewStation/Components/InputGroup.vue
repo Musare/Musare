4238cf69c1f2f2d3475bcae592f9c66d65c67386
Owen Diffey <owen@diffey.dev>
2025-06-21 13:45:41 +0100

    feat: Add input group component

---
+++ frontend/src/pages/NewStation/Components/InputGroup.vue
@@ -0,0 +1,63 @@
+<script lang="ts" setup>
+withDefaults(
+	defineProps<{
+		is?: string | object;
+	}>(),
+	{
+		is: "div"
+	}
+);
+</script>
+
+<template>
+	<component :is="is" class="input_group">
+		<slot />
+	</component>
+</template>
+
+<style lang="less" scoped>
+.input_group {
+	display: flex;
+
+	:deep(&__expanding) {
+		flex-grow: 1;
+		min-width: 0;
+	}
+
+	& > :deep(.btn) {
+		align-self: end;
+		border-radius: 0;
+
+		&:first-child:not(:last-child) {
+			border-radius: 5px 0 0 5px;
+		}
+
+		&:last-child:not(:first-child) {
+			border-radius: 0 5px 5px 0;
+		}
+	}
+
+	& > :deep(.inpt) {
+		.inpt__input {
+			border-radius: 0;
+		}
+
+		&:first-child:not(:last-child) .inpt__input {
+			border-radius: 5px 0 0 5px;
+		}
+
+		&:last-child:not(:first-child) .inpt__input {
+			border-radius: 0 5px 5px 0;
+		}
+
+		&:not(:first-child) .inpt__label {
+			padding-left: 5px;
+			border-left: solid 1px var(--light-grey-1);
+		}
+
+		&:not(:last-child) .inpt__input {
+			border-right-width: 0;
+		}
+	}
+}
+</style>
